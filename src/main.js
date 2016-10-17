e5b5f016fbc33a6c875bc84759346d8a83793667
馒头的包租公 <410780496@qq.com>
2016-10-17 23:48:15 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import Router from './router'
+/*import sync from 'vuex-router-sync'*/
 import VueTouch from 'vue-touch'
 import VueResource from 'vue-resource'
+//import store from './vuex/store'
 
 
 
@@ -11,7 +13,8 @@ import VueResource from 'vue-resource'
 Vue.config.devtools = true;
 
 Vue.use(VueResource);
-Vue.use(VueTouch)
+Vue.use(VueTouch);
+/*Vue.use(sync);*/
 //Vue.use(VueAnimatedList);
 
 Vue.http.options.root = '/static';
@@ -19,4 +22,5 @@ Vue.http.headers.common['Authorization'] = 'Basic YXBpOnBhc3N3b3Jk';
 Vue.http.options.emulateJSON = true;
 
 
+/*sync(store, Router);*/
 Router.start(App, '#app');
