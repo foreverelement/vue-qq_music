f88a80810fd84b7a412274de437e9da2f62a1a43
馒头的包租公 <410780496@qq.com>
2016-10-17 11:43:31 +0800

    增加首页选项卡容器

---
+++ src/items/item1.vue
@@ -0,0 +1,106 @@
+<template>
+	<btn></btn>
+	<div class="mui-table-view user">
+		<div class="mui-table-view-cell mui-media">
+			<a class="mui-navigate-right">
+				<img class="mui-media-object mui-pull-left" src="/static/img/3.jpg">
+				<div class="mui-media-body">
+					<h5>陆溢昕</h5>
+					<p class="mui-ellipsis">今日听歌3小时</p>
+				</div>
+				<img src="/static/img/vip.png" class="mui-pull-right">
+			</a>
+		</div>
+	</div>
+	<grid></grid>
+	<div class="mui-table-view radio">
+		<div class="mui-table-view-cell mui-media">
+			<div class="mui-pull-left cover-box">
+				<img class="mui-media-object" src="/static/img/4.jpg">
+				<img src="/static/img/start.png" class="start">
+			</div>
+			<div class="mui-media-body">
+				个性电台
+				<p class="mui-ellipsis">听专属你的音乐</p>
+			</div>
+		</div>
+	</div>
+	<list></list>
+	<player></player>
+</template>
+<script>
+import btn from '../components/btn'
+import grid from '../components/grid'
+import list from '../components/list'
+import player from '../components/player'
+	export default {
+		components: {
+			btn,
+			grid,
+			list,
+			player
+		}
+	}
+</script>
+<style scoped>
+	.user {
+		margin-bottom: 10px;
+	}
+
+	.user .mui-navigate-right {
+		padding: 5px 10px;
+	}
+
+	.user img.mui-media-object {
+		border-radius: 50%;
+	}
+
+	.user h5 {
+		font-size: 14px;
+		margin-bottom: 0;
+		color: #333;
+	}
+
+	.user p {
+		font-size: 12px;
+	}
+
+	.user img.mui-pull-right {
+		margin-top: -28px;
+		max-width: 18px;
+		margin-right: 25px;
+	}
+
+	.radio {
+		margin-bottom: 10px;
+	}
+
+	.radio img.mui-media-object {
+		line-height: 56px;
+		max-width: 56px;
+		height: 56px;
+	}
+
+	.radio .cover-box {
+		position: relative;
+	}
+
+	.radio img.start {
+		width: 60%;
+		position: absolute;
+		top: 20%;
+		left: 20%;
+	}
+
+	.radio .cover-box {
+		margin-right: 10px;
+	}
+
+	.radio .mui-media-body {
+		padding-top: 5px;
+	}
+
+	.radio .mui-media-body p {
+		margin-top: 5px;
+	}
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -2,118 +2,39 @@
 	<div class="home">
 		<hd></hd>
 		<div class="mui-content">
-			<btn></btn>
-			<div class="mui-table-view user">
-				<div class="mui-table-view-cell mui-media">
-					<a class="mui-navigate-right">
-						<img class="mui-media-object mui-pull-left" src="/static/img/3.jpg">
-						<div class="mui-media-body">
-							<h5>陆溢昕</h5>
-							<p class="mui-ellipsis">今日听歌3小时</p>
-						</div>
-						<img src="/static/img/vip.png" class="mui-pull-right">
-					</a>
-				</div>
+			<div id="item1" class="mui-control-content mui-active">
+				<item1></item1>
 			</div>
-			<grid></grid>
-			<div class="mui-table-view radio">
-				<div class="mui-table-view-cell mui-media">
-					<div class="mui-pull-left cover-box">
-						<img class="mui-media-object" src="/static/img/4.jpg">
-						<img src="/static/img/start.png" class="start">
-					</div>
-					<div class="mui-media-body">
-						个性电台
-						<p class="mui-ellipsis">听专属你的音乐</p>
-					</div>
-				</div>
+			<div id="item2" class="mui-control-content">
+				<item2></item2>
+
 			</div>
-			<list></list>
+			<div id="item3" class="mui-control-content">
+				<item3></item3>
+			</div>
 		</div>
-		<player></player>
 	</div>
-	
 </template>
 <script>
+import item1 from '../items/item1'
+import item2 from '../items/item2'
+import item3 from '../items/item3'
 import hd from '../components/hd'
-import btn from '../components/btn'
-import grid from '../components/grid'
-import list from '../components/list'
-import player from '../components/player'
 	export default {
 		components: {
-			hd, btn, grid, list, player
+			hd,
+			item1,
+			item2,
+			item3,
 		}
 	}
 </script>
 <style scoped>
-
 	.mui-table-view-cell.mui-active {
 		background-color: transparent;
 	}
 
 	.home {
 		padding-bottom: 50px;
-	}
-
-	.user {
-		margin-bottom: 10px;
-	}
-
-	.user .mui-navigate-right {
-		padding: 5px 10px;
-	}
-
-	.user img.mui-media-object {
-		border-radius: 50%;
-	}
-
-	.user h5 {
-		font-size: 14px;
-		margin-bottom: 0;
-		color: #333;
-	}
-
-	.user p {
-		font-size: 12px;
-	}
-
-	.user img.mui-pull-right {
-		margin-top: -28px;
-		max-width: 18px;
-		margin-right: 25px;
-	}
-
-	.radio {
-		margin-bottom: 10px;
-	}
-
-	.radio img.mui-media-object {
-		line-height: 56px;
-		max-width: 56px;
-		height: 56px;
-	}
-
-	.radio .cover-box {
-		position: relative;
-	}
-
-	.radio img.start {
-		width: 60%;
-		position: absolute;
-		top: 20%;
-		left: 20%;
-	}
-
-	.radio .cover-box {
-		margin-right: 10px;
-	}
-
-	.radio .mui-media-body {
-		padding-top: 5px;
-	}
-
-	.radio .mui-media-body p {
-		margin-top: 5px;
 	}
 </style>
