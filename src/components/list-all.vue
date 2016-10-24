24dea51200fd6cbce86aebdef50dc6ea012f6c75
馒头的包租公 <410780496@qq.com>
2016-10-24 19:47:45 +0800

    更新UI样式

--- src/components/list-all.vue
+++ src/components/list-all.vue
@@ -1,53 +1,24 @@
 <template>
-	<div class="mui-table-view-cell title">QQ音乐巅峰榜</div>
-	<ul class="mui-table-view mui-table-view-chevron list">
-		<li class="mui-table-view-cell mui-media">
-			<a class="mui-navigate-right">
-				<div class="mui-media-object mui-pull-left">
-					<img class="cover" src="/static/img/11.jpg">
-					<div class="start">
-						<span>20.1万</span>
-						<img src="/static/img/play-1.png">
-					</div>
-				</div>
+	<div class="mui-table-view-cell title">全球榜</div>
+	<ul class="mui-table-view mui-grid-view list">
+		<li class="mui-table-view-cell mui-media mui-col-xs-4">
+			<a href="#">
+				<img class="mui-media-object" src="/static/img/11.jpg">
 				<div class="mui-media-body">
-					<p class="mui-ellipsis">1 某时某刻</p>
-					<p class="mui-ellipsis">2 蓝瘦香菇 <span>- 蒙面哥</span></p>
-					<p class="mui-ellipsis">3 童梦 <span>- 张碧晨</span></p>
+				<p class="mui-ellipsis-2">幸福就是可以一起睡觉</p>
 				</div>
 			</a>
 		</li>
-		<li class="mui-table-view-cell mui-media">
-			<a class="mui-navigate-right" href="javascript:;">
-				<div class="mui-media-object mui-pull-left">
-					<img class="cover" src="/static/img/12.jpg">
-					<div class="start">
-						<span>20.1万</span>
-						<img src="/static/img/play-1.png">
-					</div>
-				</div>
-				
-				<div class="mui-media-body">
-					<p class="mui-ellipsis">1 演员 <span>- 薛之谦</span></p>
-					<p class="mui-ellipsis">2 丑八怪 <span>- 薛之谦</span></p>
-					<p class="mui-ellipsis">3 多幸运 <span>- 韩安旭</span></p>
-				</div>
+		<li class="mui-table-view-cell mui-media mui-col-xs-4">
+			<a href="#">
+				<img class="mui-media-object" src="/static/img/11.jpg">
+				<div class="mui-media-body">想要一间这样的木屋，静静的喝咖啡</div>
 			</a>
 		</li>
-		<li class="mui-table-view-cell mui-media">
-			<a class="mui-navigate-right">
-				<div class="mui-media-object mui-pull-left">
-					<img class="cover" src="/static/img/13.jpg">
-					<div class="start">
-						<span>20.1万</span>
-						<img src="/static/img/play-1.png">
-					</div>
-				</div>
-				<div class="mui-media-body">
-					<p class="mui-ellipsis">1 告白气球(Live) <span>- 周杰伦/低调组合</span></p>
-					<p class="mui-ellipsis">2 你还要我怎样(Live) <span>- 薛之谦</span></p>
-					<p class="mui-ellipsis">3 小幸运(Live) <span>- 白静晨</span></p>
-				</div>
+		<li class="mui-table-view-cell mui-media mui-col-xs-4">
+			<a href="#">
+				<img class="mui-media-object" src="/static/img/11.jpg">
+				<div class="mui-media-body">Color of SIP CBD</div>
 			</a>
 		</li>
 	</ul>
@@ -61,62 +32,6 @@
 	}
 	ul.list {
 		background-color: transparent;
-		padding: 0 10px;
 	}
 
-	.list li.mui-media:first-child {
-		margin-top: 0;
-	}
-
-	.list li.mui-media {
-		background-color: #fff;
-		margin-top: 10px;
-		padding: 0;
-	}
-
-	.list li.mui-media a {
-		margin: 0;
-	}
-
-	.list li.mui-media .mui-media-body {
-		margin-top: 1.4rem;
-	}
-
-	.list li.mui-media a p {
-		width: 80%;
-		color: #333;
-	}
-
-	.list li.mui-media a p span {
-		color: #999;
-	}
-
-	.list li.mui-media .mui-media-object {
-		max-width: 100px;
-		height: 100px;
-		line-height: 100px;
-		position: relative;
-	}
-	.list li.mui-media .mui-media-object img.cover {
-		width: 100%;
-	}
-
-	.list .start {
-		position: absolute;
-		width: 100%;
-		bottom: 0;
-		line-height: 15px;
-	}
-
-	.list .start span {
-		color: #fff;
-		font-size: .6rem;
-		vertical-align: -.5rem;
-		margin-left: 4px;
-	}
-
-	.list .start img {
-		width: 25%;
-		float: right;
-	}
 </style>
